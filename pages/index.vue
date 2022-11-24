851166671c0fa9ab3daa083483db4ea784dd5451
nirav <nirav.gaglani@improwised.com>
2022-11-24 15:33:14 +0530

    Added Search Panel

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,13 @@
 <template>
     <div>
+        <SearchPanel />
     </div>
 </template>
+
+<style>
+main{ 
+        background: linear-gradient(rgba(25, 25, 25, 0.7),
+                       rgba(25, 25, 25, 0.7)), url("../assets/background.jpg")no-repeat center center/cover;
+        height: 50vh;
+    }
+</style>
