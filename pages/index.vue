7a8553b9c77d25daecc26f171f198c79213b347c
nirav <nirav.gaglani@improwised.com>
2022-11-24 15:34:14 +0530

    Improved code-format

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,8 @@
 
 <style>
 main{ 
-        background: linear-gradient(rgba(25, 25, 25, 0.7),
-                       rgba(25, 25, 25, 0.7)), url("../assets/background.jpg")no-repeat center center/cover;
-        height: 50vh;
-    }
+    background: linear-gradient(rgba(25, 25, 25, 0.7),
+                    rgba(25, 25, 25, 0.7)), url("../assets/background.jpg")no-repeat center center/cover;
+    height: 50vh;
+}
 </style>
