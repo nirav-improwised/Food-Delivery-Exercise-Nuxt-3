0f07d0e2c792920e49a94b86d1b8e422e4e3c3b8
nirav <nirav.gaglani@improwised.com>
2022-11-28 12:20:21 +0530

    Improved responsiveness of filters

--- components/Filters.vue
+++ components/Filters.vue
@@ -1,52 +1,39 @@
 <template>
-    <section class="container-lg mt-3 mb-4 pt-1">
-    <div id="filters" class="fs-3 row d-flex mt-1">
+<section class="container-lg mt-3 mb-4 pt-1">
+<div id="filters" class="fs-3 row d-flex mt-1">
+        <div class="text-danger mb-1">
+            Help Us Select Your Preferences
+        </div>
     
-        <div class="text-danger mb-1">Help Us Select Your Preferences</div>
-        <HorizontalRule />
-    
-        <div class="col-6 d-flex justify-content-between mt-3">
-            
-            <input type="checkbox" class="btn-check" id="delivering" name="Has Online Delivery">
+        <div class="justify-content-between mt-2">
+            <input type="checkbox" class="btn-check mt-1" id="delivering" name="Has Online Delivery">
             <label for="delivering" class="btn btn-danger me-2">Delivering</label>
     
-            <input type="checkbox" class="btn-check" id="reservations" name="Has Table booking">
+            <input type="checkbox" class="btn-check mt-1" id="reservations" name="Has Table booking">
             <label for="reservations" class="btn btn-danger me-2">Advanced Bookings</label>
-    
- <!-- <select placeholder="City" class="form-control" id="exampleFormControlSelect1" name="city"> -->
-                      <!-- <option>Nirav</option> -->
-                      <!-- <option selected>London</option>
-                      <option>Boston</option>
-                      <option>Mumbai</option>
-                      <option>New York</option>
-                      <option>Toronto</option>
-                      <option>Paris</option> -->
-                  <!-- </select> -->
 
-            <select class="btn btn-danger dropdown-toggle me-2" placeholder="Cuisine" type="button" name="Cuisines" data-bs-toggle="dropdown" aria-expanded="false">
-                <!-- <option>Cuisine</option> -->
+            <select class="btn btn-danger dropdown-toggle me-2 mt-1" placeholder="Cuisine" type="button" name="Cuisines" data-bs-toggle="dropdown" aria-expanded="false">
+                <option selected value="not_selected">Cuisine</option>
                 <option>Indian</option>
                 <option>Thai</option>
                 <option>Japanese</option>
             </select>
     
-            <select class="btn btn-danger dropdown-toggle me-2" placeholder="Currency" type="button" name="Currency" data-bs-toggle="dropdown" aria-expanded="false">
-                <!-- <option>Currency</option> -->
+            <select class="btn btn-danger dropdown-toggle me-2 mt-1" placeholder="Currency" type="button" name="Currency" data-bs-toggle="dropdown" aria-expanded="false">
+                <option selected value="not_selected">Currency</option>
                 <option>INR</option>
                 <option>USD</option>
                 <option>RUB</option>
             </select>
     
-            <select class="btn btn-danger dropdown-toggle me-2" placeholder="Ratings" type="button" name="Ratings" data-bs-toggle="dropdown" aria-expanded="false">
-                <!-- <option>Ratings</option> -->
+            <select class="btn btn-danger dropdown-toggle me-2 mt-1" placeholder="Ratings" type="button" name="Ratings" data-bs-toggle="dropdown" aria-expanded="false">
+                <option selected value="not_selected">Ratings</option>
                 <option>4+</option>
                 <option>3+</option>
                 <option>2+</option>
                 <option>1+</option>
-            </select>
-            
+            </select>  
         </div>
-    
-    </div>
-    </section>
-    </template>
+</div>
+</section>
+</template>
--- components/SearchPanel.vue
+++ components/SearchPanel.vue
@@ -5,7 +5,7 @@
         <div class="mt-4 pt-4 w-50 text-white">
             <h1>Discover Best Restaurants Delevering near you</h1>
         </div>
-          <div class="row d-flex justify-content-between h3 mt-lg-5 mt-sm-2 pt-md-5 pt-sm3">
+          <div class="row d-flex h3 mt-lg-5 mt-sm-2 pt-md-5 pt-sm-3 ms-1">
              <form id="search-form">
                 <div class="col-md-2 col-3 p-0 d-inline-block">
                   <select class="form-control" id="exampleFormControlSelect1" name="city">
