0edc3f715d01f3cf1905bc450077124702f46632
nirav <nirav.gaglani@improwised.com>
2022-11-29 11:22:00 +0530

    Removed border, added ratings, changed card-body

--- components/List.vue
+++ components/List.vue
@@ -1,18 +1,19 @@
 <template>
 <section class="container-lg mt-1 mb-4 pt-1">
-    <div class="row mt-2 d-flex justify-content-between">
-        <div v-for="i, index in 20" :key="index" class="card col-md-3 col-xs-4 p-3 border border-2 mt-4">
-                <img src="../assets/card-1-image.png" alt="" class="card-img-top image-fluid mt-2" width="100%" height="auto">
-                <h3 class="card-title">{{props.data[i].RestaurantName}}</h3>
-                <h6 class="card-subtitle text-muted">{{props.data[i].Address}}</h6>
-                <div class="card-body p-0 pt-2 mb-2">
+    <div class="row row-cols-lg-4 row-cols-sm-3 row-cols-xs-2 row-cols-1">
+        <div v-for="i, index in 20" :key="index" class="col">
+            <div class="card mt-4 border border-white">
+                <img src="../assets/card-1-image.png" alt="" class="card-img-top">
+                <div class="card-body p-1 pt-2 mb-2 position-relative">
+                        <div class="h6 w-75">{{data[i].RestaurantName}}</div>
+                        <span class="card-text bg bg-success d-inine px-1 py-1 rounded-1 text-white position-absolute top-0 end-0 me-1 mt-2 fs-6">&#9733; {{data[i].AggregateRating}}</span>
                     <ul class="list-unstyled">
-                        <li>Restaurant Status: Available</li>
-                        <li>Sr.No. {{props.data[i].Sr_No}}</li>
-                        <li>Votes: 1256</li>
+                        <li class="card-text">{{data[i].Cuisines}}</li>
                     </ul>
-                    <a href="#" class="btn btn-danger">Order</a>
+                    <a href="#" class="btn btn-danger p-1">Order</a>
+                    <span class="card-text text-muted ms-3 position-absolute bottom-0 fs-5 end-0 me-1">{{data[i].AverageCost}} FOR TWO</span>
                 </div>
+            </div>
         </div>
     </div>
 </section>
