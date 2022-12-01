d9431d10be2a7bc2d9927fad1745153cec95e772
nirav <nirav.gaglani@improwised.com>
2022-12-01 17:20:36 +0530

    User can now search by city location

--- components/SearchPanel.vue
+++ components/SearchPanel.vue
@@ -1,70 +1,84 @@
 <template>
-    <main class="mt-0 mb-1 pt-1">
-      <section class="bgSPImage">
-        <div class="container-lg">
-          <div class="mt-4 pt-4 w-50 text-white">
-              <h1>Discover Best Restaurants Delevering near you</h1>
-          </div>
-            <div class="row d-flex h3 mt-lg-5 mt-sm-2 pt-md-5 pt-sm-3 ms-1">
-                  <div class="col-md-2 col-3 p-0 d-inline-block">
-  
-                  <select class="form-select" id="select_city" name="location">
-                        <option v-for="(city, index) in cityList" :key="index" :selected= "city=='Rajkot'">{{city}}</option>
-                  </select>
-  
-                  </div>
-                  <div class="col-md-4 col-3 p-0 d-inline-block">
-                      <input type="text" placeholder="Search Cuisine" class="form-control" id="search" name="cuisine">
-                  </div>
-                  <div class="col-1 p-0 d-inline-block">
-                      <button type="submit" class="btn btn-danger ms-1">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
-                      </button>
-                  </div>
-          </div>
-      </div>
-    </section>
-  </main>
-  </template>
-  
-  <style>
-  .bgSPImage{ 
-      background: linear-gradient(rgba(25, 25, 25, 0.7),
-                      rgba(25, 25, 25, 0.7)), url("../assets/background.jpg")no-repeat center center/cover;
-      height: 50vh;
-  }
-  </style>
-  
-  <script setup>
-  const rD = require('../composables/removeDuplicates.js');
-  
-  const props = defineProps({
-    data: Array
-  });
-  let duplicatedCityList, cityList;
-  duplicatedCityList = await getCityArr(props.data);
-  cityList = await rD.removeDuplicates(duplicatedCityList);
-  console.log(cityList)
-  
-  function filterByCity(dataSet, city){
-    let filteredData=[];
-    dataSet.forEach(restro => {
-        if (restro.City.includes(city)){
-            filteredData.push(restro);
-        }
+<main class="mt-0 mb-1 pt-1">
+    <section class="bgSPImage">
+    <div class="container-lg">
+        <div class="mt-4 pt-4 w-50 text-white">
+            <h1>Discover Best Restaurants Delevering near you</h1>
+        </div>
+        <div class="row d-flex h3 mt-lg-5 mt-sm-2 pt-md-5 pt-sm-3 ms-1">
+                <div class="col-md-2 col-3 p-0 d-inline-block">
+
+                <select class="form-select" v-model="tempCity" id="select_city" name="location">
+                    <option v-for="(city, index) in cityList" :key="index" :value="city">{{city}}</option>
+                </select>
+
+                </div>
+                <div class="col-md-4 col-3 p-0 d-inline-block">
+                    <input type="text" placeholder="Search Cuisine" class="form-control" id="search" name="cuisine">
+                </div>
+                <div class="col-1 p-0 d-inline-block">
+                    <button type="submit" class="btn btn-danger ms-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+                    </button>
+                </div>
+        </div>
+    </div>
+</section>
+</main>
+</template>
+
+<style>
+.bgSPImage{ 
+    background: linear-gradient(rgba(25, 25, 25, 0.7),
+                    rgba(25, 25, 25, 0.7)), url("../assets/background.jpg")no-repeat center center/cover;
+    height: 50vh;
+}
+</style>
+
+<script setup>
+
+// import { removeDuplicates } from '~~/composables/removeDuplicates.js';
+
+const router = useRouter();
+let tempCity = ref('');
+let mj = ref('');
+
+watch(tempCity, (tempCity) => {
+    router.push({
+    path: '/',
+    query: { city: tempCity },
     });
-    return filteredData
-  }
-  
-  function getCityArr(data){
-    let promise = new Promise(function(resolve, reject){
-    let restroArr=[];
-    data.forEach(restro => {
-        restroArr.push(restro.City);
-    });
-    resolve(restroArr);
-  })
-    return promise
-  }
-  </script>
-  
+    emit("selectedCity", tempCity);
+})
+
+const emit = defineEmits(['selectedCity']);
+
+// const rD = require('../composables/removeDuplicates.js');   
+
+const props = defineProps({
+data: Array
+});
+
+// Making an array of cities
+let duplicatedCityList, cityList;
+duplicatedCityList = await getCityArr(props.data);
+cityList = await removeDuplicates(duplicatedCityList);
+console.log(cityList);
+
+function getCityArr(data){
+let promise = new Promise(function(resolve, reject){
+let restroArr=[];
+data.forEach(restro => {
+    restroArr.push(restro.City);
+});
+resolve(restroArr);
+})
+return promise
+}
+
+function removeDuplicates(data) {
+    let uniQ = data.filter((item,
+        index) => data.indexOf(item) === index);
+    return uniQ
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,27 @@
 <template>
     <div>
-        <SearchPanel :data="restros"/>
+        <SearchPanel :data="restros" @selectedCity="(tempCity)=>filterByCity(tempCity)"/>
         <Filters />
-        <List :data="restros"/>
+        <List :data="filteredRestros" />
     </div>
 </template>
 
 <script setup>
-const { data: restros } = await useFetch('http://127.0.0.1:5500/restro', { mode: 'no-cors'});
+
+import { ref } from "vue";
+let { data: restros } = await useFetch('http://127.0.0.1:5500/restro', { mode: 'no-cors'});
+let filteredRestros = ref();
+filteredRestros = restros.value.slice(0,20);
+
+function filterByCity(city){
+let temp=[];
+restros.value.forEach(restro => {
+    if (restro.City.includes(city)){
+        temp.push(restro);
+    }
+});
+filteredRestros = temp;
+console.log('temp index 26', filteredRestros);
+}
+
 </script>
