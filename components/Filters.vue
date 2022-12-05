cdeb787146315cc0b1104bdd27ccca00602be5cd
nirav <nirav.gaglani@improwised.com>
2022-12-05 18:10:12 +0530

    Added dummy option to show placeholder

--- components/Filters.vue
+++ components/Filters.vue
@@ -13,14 +13,17 @@
         <label for="reservations" class="btn btn-danger me-2">Advanced Bookings</label>
 
         <select class="btn btn-danger dropdown-toggle me-2 mt-1 pt-1" v-model="filters.cuisine" placeholder="Cuisine" type="button" name="Cuisines" data-bs-toggle="dropdown" aria-expanded="false">
+            <option value="">Cuisine</option>
             <option class="bg bg-light text-dark" v-for="cuisine, index in uniQcuisines" :key="index" :value="cuisine" :selected="cuisine=='Indian'">{{cuisine}}</option>
         </select>
 
         <select class="btn btn-danger dropdown-toggle me-2 mt-1 pt-1" v-model="filters.currency" placeholder="Currency" type="button" name="Currency" data-bs-toggle="dropdown" aria-expanded="false">
+            <option value="">Currency</option>
             <option class="bg bg-light text-dark" v-for="currency, index in uniQcurrencies" :key="index" :value="currency" :selected="currency=='Indian Rupee'">{{currency}}</option>
         </select>
 
         <select class="btn btn-danger dropdown-toggle me-2 mt-1 pt-1" v-model="filters.rating" placeholder="Ratings" type="button" name="Rating" data-bs-toggle="dropdown" aria-expanded="false">
+            <option value="">Ratings</option>
             <option class="bg bg-light text-dark" v-for="i, index in 4" :key="index" :value="i" :selected="i==3">{{i}}+</option>
         </select>
         
