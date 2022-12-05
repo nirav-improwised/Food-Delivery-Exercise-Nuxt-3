014a81d65a71453145972bf573576c5750083d43
nirav <nirav.gaglani@improwised.com>
2022-12-05 12:05:22 +0530

    removed comments

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,12 @@
 <template>
     <div>
         <SearchPanel :data="restros" @search="(search)=>searchFilter(search)"/>
-        <!-- <SearchPanel :data="restros" @searchCity="(searchCity)=>filterByCity(searchCity)" @searchRestro="(searchRestro)=>filterByRestaurantName(searchRestro)"/> -->
         <Filters />
         <List :data="filteredRestros" />
     </div>
 </template>
 
 <script setup>
-
 import { ref } from "vue";
 let { data: restros } = await useFetch('http://127.0.0.1:5500/restro', { mode: 'no-cors'});
 let filteredRestros = ref();
@@ -25,30 +23,4 @@ function searchFilter(searchObject){
     filteredRestros = temp;
     }
 }
-
-// function filterByCity(city){
-//     let temp=[];
-//     // alert(city)
-//     if(city !== ""){
-//     restros.value.forEach(restro => {
-//         if (restro.City.includes(city)){
-//             temp.push(restro);
-//         }
-//     });
-//     filteredRestros = temp;
-//     }
-//     console.log('temp index 26', filteredRestros);
-// }
-
-// function filterByRestaurantName(restroName){
-//     let temp=[];
-//     alert(restroName);
-//     restros.value.forEach(restro => {
-//         if (restro.City.includes(restroName)){
-//             temp.push(restro);
-//         }
-//     });
-//     filteredRestros = temp;
-// }
-
 </script>
